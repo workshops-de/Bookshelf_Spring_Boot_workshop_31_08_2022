9a28359cd5f483d0dd6ac27da8db0e600f4e99a9
Björn Wilmsmann <bjoern@wilmsmann.de>
2022-09-06 22:41:43 +0200

    Getting started with MongoDB

--- src/main/java/de/workshops/bookshelf/user/BookshelfUserRepository.java
+++ src/main/java/de/workshops/bookshelf/user/BookshelfUserRepository.java
@@ -4,5 +4,5 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends MongoRepository<User, String> {
+public interface BookshelfUserRepository extends MongoRepository<BookshelfUser, String> {
 }
