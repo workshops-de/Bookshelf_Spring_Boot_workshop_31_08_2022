0c8c91e813625c8596b267acc29b73eb2b70a3b4
Björn Wilmsmann <bjoern@wilmsmann.de>
2022-09-07 12:29:47 +0200

    Solution for day 6

--- src/main/java/de/workshops/bookshelf/config/WebSecurityConfig.java
+++ src/main/java/de/workshops/bookshelf/config/WebSecurityConfig.java
@@ -1,19 +1,26 @@
 package de.workshops.bookshelf.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.Collections;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
+@RequiredArgsConstructor
 public class WebSecurityConfig {
+
+    private final JdbcTemplate jdbcTemplate;
 
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -38,15 +45,22 @@ public class WebSecurityConfig {
     }
 
     @Bean
-    UserDetailsService userDetailsService() {
-        var user = User.builder().username("user").password("password").roles("USER").build();
-        var admin = User.builder().username("admin").password("admin").roles("USER", "ADMIN").build();
+    public UserDetailsService userDetailsService() {
+        return username -> {
+            String sql = "SELECT * FROM bookshelf_user WHERE username = ?";
 
-        return new InMemoryUserDetailsManager(user, admin);
+            return jdbcTemplate.queryForObject(sql, (rs, rowNum) -> new User(
+                    rs.getString("username"),
+                    rs.getString("password"),
+                    Collections.singletonList(
+                            new SimpleGrantedAuthority(rs.getString("role"))
+                    )
+            ), username);
+        };
     }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
+        return new BCryptPasswordEncoder();
     }
 }
