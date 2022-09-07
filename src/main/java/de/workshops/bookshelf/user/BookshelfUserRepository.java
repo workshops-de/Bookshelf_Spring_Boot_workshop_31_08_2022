2368423ef5cc369d44e6fb08dfc4b79fbf640bbf
Björn Wilmsmann <bjoern@wilmsmann.de>
2022-09-07 11:31:41 +0200

    Reverting MongoDB requirements

--- src/main/java/de/workshops/bookshelf/user/BookshelfUserRepository.java
+++
@@ -1,8 +0,0 @@
-package de.workshops.bookshelf.user;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BookshelfUserRepository extends MongoRepository<BookshelfUser, String> {
-}
